353dec83b385b1c9224d749e2a44ffeb3e6cc7a7
Joaquín Moreno Fernández <joaquin.moreno@grupoesfera.com.ar>
2019-06-28 17:02:29 -0500

    Termina ejercicio 8 y 10 de guia 07

--- guia07/ejercicio08.py
+++ guia07/ejercicio08.py
@@ -1,4 +1,4 @@
-from scipy.fftpack import fft
+from scipy.fftpack import fft, fftshift, fftfreq
 import numpy
 import matplotlib.pyplot as plot
 
@@ -43,24 +43,46 @@ def window_tft_centered(tft):
     return tft_list[center:] + tft_list[:center]
 
 
+def window_in_db(fft):
+
+    fft_norm = abs(fft) / max(abs(fft))
+
+    return 20 * numpy.log10(fft_norm)
+
+
 t = numpy.linspace(-2 * numpy.pi, 2 * numpy.pi, 50)
-t2 = numpy.linspace(-2 * numpy.pi, 2 * numpy.pi, 1000)
+t2 = numpy.linspace(-2 * numpy.pi, 2 * numpy.pi, 10000)
+
+
 box = box_window(len(t), 0, 30)
-box_ft = fft(box, 1000)
+box_ft = fft(box, 10000)
+box_phase = numpy.angle(box_ft)
+box_dbs = window_in_db(box_ft)
 
 triangle = triangle_window(len(t), 0, 30)
-triangle_ft = fft(triangle, 1000)
+triangle_ft = fft(triangle, 10000)
+triangle_phase = numpy.angle(triangle_ft)
+triangle_dbs = window_in_db(triangle_ft)
 
 hamming = hamming_window(len(t), 0, 30)
-hamming_ft = fft(hamming, 1000)
+hamming_ft = fft(hamming, 10000)
+hamming_phase = numpy.angle(hamming_ft)
+hamming_dbs = window_in_db(hamming_ft)
 
-_, subplot = plot.subplots(3, 3)
+
+_, subplot = plot.subplots(3, 4)
 
 subplot[0][0].stem(t, box)
 subplot[0][0].title.set_text("Ventana Rectangular")
 
 subplot[0][1].plot(t2, window_tft_centered(abs(box_ft)))
-subplot[0][1].title.set_text("Transformada ventana rectangular")
+subplot[0][1].title.set_text("Transformada Ventana Rectangular")
+
+subplot[0][2].plot(t2, window_tft_centered(box_dbs))
+subplot[0][2].title.set_text("Transformada Ventana Rectangular dBs")
+
+subplot[0][3].plot(t2, window_tft_centered(abs(box_phase)))
+subplot[0][3].title.set_text("Fase Ventana Rectangular")
 
 subplot[1][0].stem(t, triangle)
 subplot[1][0].title.set_text("Ventana Triangular")
@@ -68,11 +90,22 @@ subplot[1][0].title.set_text("Ventana Triangular")
 subplot[1][1].plot(t2, window_tft_centered(abs(triangle_ft)))
 subplot[1][1].title.set_text("Transformada Ventana Triangular")
 
+subplot[1][2].plot(t2, window_tft_centered(triangle_dbs))
+subplot[1][2].title.set_text("Transformada Ventana Triangular dBs")
+
+subplot[1][3].plot(t2, window_tft_centered(abs(triangle_phase)))
+subplot[1][3].title.set_text("Fase ventana Triangular")
+
 subplot[2][0].stem(t, hamming)
 subplot[2][0].title.set_text("Ventana Hamming")
 
 subplot[2][1].plot(t2, window_tft_centered(abs(hamming_ft)))
 subplot[2][1].title.set_text("Transformada Ventana Hamming")
 
-plot.tight_layout()
+subplot[2][2].plot(t2, window_tft_centered(hamming_dbs))
+subplot[2][2].title.set_text("Transformada Ventana Triangular dBs")
+
+subplot[2][3].plot(t2, window_tft_centered(abs(hamming_phase)))
+subplot[2][3].title.set_text("Fase Ventana Hamming")
+
 plot.show()
